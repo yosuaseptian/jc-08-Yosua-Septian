f4da1f9c92f4eb8955239131e832e17350a2a974
rochafialvin <rochafi.dev@gmail.com>
2019-05-15 12:09:28 +0700

    Drawer with style

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 class SideDrawer extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Side Drawer</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 22,
+        backgroundColor: 'white',
+        flex: 1
+    }
+})
 
 export default SideDrawer
